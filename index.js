8c8ec1fcb88f4b1c7656a0a3f959d93d90dff315
ConsoleMage <128290297+ConsoleMage@users.noreply.github.com>
2024-12-02 13:30:15 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let pEl = document.getElementById("p-el");
+let count = 0;    
+let colorIndex = 0;
+const colors = ["#ff006e", "#3a86ff", "#ffbe0b", "#e9ecef4"];
+
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+
+function getNextColor() { 
+    colorIndex = (colorIndex + 1) % colors.length; 
+    return colors[colorIndex]; 
+} 
+
+function save() { 
+    let countStr = count; 
+    const container = document.getElementById("p-container"); 
+    const newElement = document.createElement("p"); 
+    
+    newElement.className = "save-element"; 
+    newElement.textContent = countStr; 
+    newElement.style.color = getNextColor(); 
+    container.appendChild(newElement);
+    
+    count = 0; 
+}
